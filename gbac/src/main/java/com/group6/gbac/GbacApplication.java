c60c4a819fc00573d3fe20e5917cde50d7b553d2
jordanzhancock <jordanzhancock@hotmail.com>
2024-03-19 16:00:27 -0400

    First commit

---
+++ gbac/src/main/java/com/group6/gbac/GbacApplication.java
@@ -0,0 +1,13 @@
+package com.group6.gbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbacApplication.class, args);
+	}
+
+}
---
+++ gbac/src/test/java/com/group6/gbac/GbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.gbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
